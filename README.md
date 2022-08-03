fb97ec17e643fe6ea8d39e5f50439af0056c8f53
Duane Irvin <irvin93d@gmail.com>
2022-08-03 11:22:25 +0200

    Create creamy icecreams

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 
 This repo contains examples of how you can use quickcheck (the JS library is called fast-check)
 to generate random data to test your code.
+
+## Prerequisites
+
+Have node and npm installed. You probably want to have some basic typescript + unit-testing knowledge as well :)
+
+## Getting started
+
+1. Install dependencies
+
+```bash
+npm install
+```
+
+2. Run the test suite in watch mode
+
+```bash
+npm test
+```
+
+3. Start hacking!
+
+
+## Quick access QR code
+
+![https://github.com/irvin93d/generate-tests-with-javascript](./qr-code.png "https://github.com/irvin93d/generate-tests-with-javascript")
--- src/icecream.ts
+++ src/icecream.ts
@@ -1,3 +1,61 @@
-export function identity(x: any) {
-  return x;
+//
+// Interfaces
+//
+
+export interface IceCreamV1 {
+  name: string;
+  /**
+   * value 1,2,3,4,5
+   */
+  deliciousness: number;
+  isCreamy: boolean;
+  keywords: string[];
+}
+
+export interface IceCreamV2 {
+  name: string;
+  /**
+   * value between 1 - 100
+   */
+  deliciousness: number;
+  /**
+   * value between 1 - 100
+   */
+  creaminess: number;
+  keywords: string[];
+}
+
+//
+// Migrations
+//
+
+export function migrate(icecream: IceCreamV1): IceCreamV2 {
+  let creaminess = 0;
+  if (icecream.keywords.includes("mega creamy")) {
+    creaminess = 100;
+  } else if (icecream.isCreamy) {
+    creaminess = 50;
+  } else if (icecream.keywords.includes("creamy")) {
+    creaminess = 50;
+  }
+
+  const deliciousness = icecream.deliciousness * 20;
+
+  return {
+    creaminess,
+    deliciousness,
+    keywords: icecream.keywords,
+    name: icecream.name,
+  };
+}
+
+export function rollback(icecream: IceCreamV2): IceCreamV1 {
+  return {
+    name: icecream.name,
+    // BUG: isCreamy: icecream.creaminess >= 50,
+    isCreamy: icecream.creaminess === 50,
+    // BUG: deliciousness: Math.ceil(icecream.deliciousness / 20),
+    deliciousness: icecream.deliciousness / 20,
+    keywords: icecream.keywords,
+  };
 }
