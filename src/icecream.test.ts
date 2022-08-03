02b173ff3a9ced0e9c79cc8f130d002f95330e33
Duane Irvin <irvin93d@gmail.com>
2022-08-03 14:47:23 +0200

    Slimmer for lightning talk

--- src/icecream.test.ts
+++ src/icecream.test.ts
@@ -6,104 +6,51 @@ import fc from "fast-check";
 
 fc.configureGlobal({ numRuns: 100, verbose: fc.VerbosityLevel.VeryVerbose });
 
-const twisterV1: IceCreamV1 = {
-  name: "Twister",
-  deliciousness: 5,
-  isCreamy: false,
-  keywords: ["the best"],
-};
-const twisterV2: IceCreamV2 = {
-  name: "Twister",
-  deliciousness: 100,
-  creaminess: 0,
-  keywords: ["the best"],
-};
-
-const magnumV1 = {
-  name: "Magnum",
-  deliciousness: 3,
-  isCreamy: true,
-  keywords: ["creamy"],
-};
-const magnumV2 = {
-  name: "Magnum",
-  deliciousness: 60,
-  creaminess: 50,
-  keywords: ["creamy"],
-};
-
 /**
  * Basic unit-test. Tests one case.
  */
 test("should migrate and rollback correctly", () => {
-  const icecreamV1 = twisterV1;
-  const icecreamV2 = twisterV2;
+  const icecreamV1: IceCreamV1 = {
+    name: "Twister",
+    isCreamy: false,
+    keywords: ["the best"],
+  };
+  const icecreamV2: IceCreamV2 = {
+    name: "Twister",
+    creaminess: 0,
+    keywords: ["the best"],
+  };
 
+  // Test migration
   const migratedIcecream = migrate(icecreamV1);
   expect(migratedIcecream).toStrictEqual(icecreamV2);
 
+  // Test rollback
   const rolledbackIceCream = rollback(migratedIcecream);
   expect(rolledbackIceCream).toStrictEqual(icecreamV1);
-});
-
-/**
- * Table test. Reuses the same test with different data.
- */
-test.each([
-  [twisterV1, twisterV2],
-  [magnumV1, magnumV2],
-])("should migrate and rollback correctly for %o", (icecreamV1, icecreamV2) => {
-  const migratedIcecream = migrate(icecreamV1);
-  expect(migratedIcecream).toStrictEqual(icecreamV2);
-
-  const rolledbackIceCream = rollback(migratedIcecream);
-  expect(rolledbackIceCream).toStrictEqual(icecreamV1);
-});
-
-/**
- * Keyword generator. Creates a random keyword from a some options.
- */
-const arbitraryKeyword = fc.constantFrom(
-  "creamy",
-  "mega creamy",
-  "icey",
-  "super icey",
-  "colors",
-  "rainbow",
-  "quick melter",
-  "just for instagram"
-);
-
-/**
- * IceCreamV2 generator. Creates a random ice-cream of V2.
- */
-const arbitraryIceCreamV2 = fc.record({
-  name: fc.string(),
-  deliciousness: fc.integer({ min: 1, max: 100 }),
-  creaminess: fc.integer({ min: 1, max: 100 }),
-  keywords: fc.array(arbitraryKeyword),
-});
-
-/**
- * Property test. For any rollback, invalid values will not be produced.
- */
-test.skip("rollback(data) does not produce invalid values", () => {
-  fc.assert(
-    fc.property(arbitraryIceCreamV2, (icecreamV2) => {
-      const icecreamV1 = rollback(icecreamV2);
-      expect(icecreamV1.deliciousness).to.be.oneOf([1, 2, 3, 4, 5]);
-    })
-  );
 });
 
 /**
  * IceCreamV1 generator. Creates a random ice-cream of V1.
  */
 const arbitraryIceCreamV1 = fc.record({
+  // Create a random string
   name: fc.string(),
-  deliciousness: fc.oneof(fc.constantFrom(1, 2, 3, 4, 5)),
+  // Either true or false
   isCreamy: fc.boolean(),
-  keywords: fc.array(arbitraryKeyword),
+  // Some of the following keywords
+  keywords: fc.array(
+    fc.constantFrom(
+      "creamy",
+      "mega creamy",
+      "icey",
+      "super icey",
+      "colors",
+      "rainbow",
+      "quick melter",
+      "just for instagram"
+    )
+  ),
 });
 
 /**
--- src/icecream.ts
+++ src/icecream.ts
@@ -4,23 +4,12 @@
 
 export interface IceCreamV1 {
   name: string;
-  /**
-   * value 1,2,3,4,5
-   */
-  deliciousness: number;
   isCreamy: boolean;
   keywords: string[];
 }
 
 export interface IceCreamV2 {
   name: string;
-  /**
-   * value between 1 - 100
-   */
-  deliciousness: number;
-  /**
-   * value between 1 - 100
-   */
   creaminess: number;
   keywords: string[];
 }
@@ -29,33 +18,25 @@ export interface IceCreamV2 {
 // Migrations
 //
 
-export function migrate(icecream: IceCreamV1): IceCreamV2 {
+export function migrate({ name, isCreamy, keywords }: IceCreamV1): IceCreamV2 {
   let creaminess = 0;
-  if (icecream.keywords.includes("mega creamy")) {
+  if (keywords.includes("mega creamy")) {
     creaminess = 100;
-  } else if (icecream.isCreamy) {
+  } else if (keywords.includes("creamy")) {
     creaminess = 50;
-  } else if (icecream.keywords.includes("creamy")) {
+  } else if (isCreamy) {
     creaminess = 50;
   }
 
-  const deliciousness = icecream.deliciousness * 20;
-
-  return {
-    creaminess,
-    deliciousness,
-    keywords: icecream.keywords,
-    name: icecream.name,
-  };
+  return { name, keywords, creaminess };
 }
 
-export function rollback(icecream: IceCreamV2): IceCreamV1 {
-  return {
-    name: icecream.name,
-    // BUG: isCreamy: icecream.creaminess >= 50,
-    isCreamy: icecream.creaminess === 50,
-    // BUG: deliciousness: Math.ceil(icecream.deliciousness / 20),
-    deliciousness: icecream.deliciousness / 20,
-    keywords: icecream.keywords,
-  };
+export function rollback({
+  name,
+  creaminess,
+  keywords,
+}: IceCreamV2): IceCreamV1 {
+  // BUG: isCreamy: icecream.creaminess >= 50,
+  const isCreamy = creaminess === 50;
+  return { name, isCreamy, keywords };
 }
